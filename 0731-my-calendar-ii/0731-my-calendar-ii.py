9280ff070b58143b0b9abc0c99d838f7dc828075
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-09-16 15:21:08 -0400

    Time: 1046 ms (34.52%), Space: 18.8 MB (23.35%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -0,0 +1,30 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.delta = defaultdict(int)
+        
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        self.delta[startTime] += 1
+        self.delta[endTime] -= 1
+        
+        running = 0
+        for x in sorted(self.delta):
+            running += self.delta[x]
+            if running > 2:
+                self.delta[startTime] -= 1
+                self.delta[endTime] += 1
+                if self.delta[startTime] == 0:
+                    del self.delta[startTime]
+                if self.delta[endTime] == 0:
+                    del self.delta[endTime]
+                
+                return False
+        return True
+
+        
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(startTime,endTime)
