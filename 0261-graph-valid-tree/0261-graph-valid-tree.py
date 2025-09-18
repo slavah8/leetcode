9d52b2efdbb62232d1acd66897f2757fbdd40a4d
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-09-18 13:33:00 -0400

    Time: 0 ms (100%), Space: 18.8 MB (84.95%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -0,0 +1,40 @@
+class DSU:
+    def __init__(self, N):
+        self.parent = [i for i in range(N)]
+        self.rank = [1] * N
+        self.components = N
+    
+    def find(self, x):
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+    
+    def union(self, a, b):
+        root_a, root_b = self.find(a), self.find(b)
+
+        if root_a == root_b:
+            return False
+        
+        if self.rank[root_a] < self.rank[root_b]:
+            root_a, root_b = root_b, root_a
+
+        self.parent[root_b] = root_a
+
+        if self.rank[root_a] == self.rank[root_b]:
+            self.rank[root_a] += 1
+        self.components -= 1
+        return True
+
+        
+class Solution:
+    def validTree(self, N: int, edges: List[List[int]]) -> bool:
+        dsu = DSU(N)
+        for u, v in edges:
+            if not dsu.union(u, v):
+                return False
+        
+        return dsu.components == 1
+                
+
+        
