552a9c0b576a59a3529fca83256058a67c5e4d3d
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-09-19 17:50:02 -0400

    Time: 441 ms (50.18%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE skip INT;
+  SET skip = N - 1;
+  RETURN (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT skip, 1
+  );
+END;
