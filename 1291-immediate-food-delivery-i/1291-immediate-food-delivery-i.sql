872f7eca9b0e3574a7ca6547f36abed2248f6954
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-09-24 16:00:17 -0400

    Time: 441 ms (50.59%), Space: 0B (100%) - LeetHub

---
+++ 1291-immediate-food-delivery-i/1291-immediate-food-delivery-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+
+SELECT
+ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1.0 ELSE 0 END) * 100, 2) AS immediate_percentage
+FROM Delivery
