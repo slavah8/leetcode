cfcceb0377308d39ca64a49cde2fe767cccc56dc
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-10-08 16:53:09 -0400

    Time: 127 ms (32.88%), Space: 44.4 MB (6.85%) - LeetHub

---
+++ 1832-minimum-operations-to-make-a-subsequence/1832-minimum-operations-to-make-a-subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minOperations(self, target: List[int], arr: List[int]) -> int:
+        
+        pos = {x : i for i, x in enumerate(target)}
+        idx_seq = []
+        for x in arr:
+            if x in pos:
+                idx_seq.append(pos[x])
+        print(idx_seq)
+
+        tails = []
+
+        for v in idx_seq:
+            i = bisect.bisect_left(tails, v)
+
+            if i == len(tails):
+                tails.append(v)
+            else:
+                tails[i] = v
+        return len(target) - len(tails)
