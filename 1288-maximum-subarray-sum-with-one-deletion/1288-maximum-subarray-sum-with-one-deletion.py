3c3f5c0f730ecf289bc976345d809ebe404d1ffe
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-10-08 19:07:57 -0400

    Time: 51 ms (42.96%), Space: 31.6 MB (41.48%) - LeetHub

---
+++ 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumSum(self, arr: List[int]) -> int:
+        # dp[i] = max subarray sum up to index i with one deletion
+        N = len(arr)
+        INF = 10 ** 10
+        dp0 = [0] * N  # max subarray sum up to index i without deletion
+        dp1 = [0] * N # max subarray sum up to index i with deletion
+        dp0[0] = arr[0]
+        dp1[0] = -INF
+        
+        best = dp0[0]
+        for i in range(1, N):
+            x = arr[i]
+            dp0[i] = max(x, dp0[i - 1] + x) # start new or continue 
+            dp1[i] = max(dp0[i - 1], dp1[i - 1] + x)
+            best = max(best, dp0[i], dp1[i])
+        return best
