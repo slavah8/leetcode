c327b61f21244e698b88725d3c69e233e1b353d2
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-10-20 15:33:16 -0400

    Time: 79 ms (43.3%), Space: 17.9 MB (98.61%) - LeetHub

---
+++ 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def addBoldTag(self, s: str, words: List[str]) -> str:
+        N = len(s)
+        bold = [False] * N
+
+        for i in range(N):
+            end = i
+            for w in words:
+                if s.startswith(w, i):
+                    end = max(end, i + len(w))
+            for j in range(i, end):
+                bold[j] = True
+        
+        i = 0
+        res = []
+        while i < N:
+            if not bold[i]:
+                res.append(s[i])
+                i += 1
+            else:
+                # start a bolding
+                res.append('<b>')
+                j = i
+                while j < N and bold[j]:
+                    res.append(s[j])
+                    j += 1
+                res.append('</b>')
+                i = j
+        return ''.join(res)
