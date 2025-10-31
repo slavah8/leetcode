771dc96aa27e5cfc2e6bb85039b27d802ffdf374
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-10-31 18:48:13 -0400

    Time: 91 ms (81.13%), Space: 18.2 MB (63.58%) - LeetHub

---
+++ 0831-largest-sum-of-averages/0831-largest-sum-of-averages.py
@@ -0,0 +1,30 @@
+class Solution:
+    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
+        n = len(nums)
+
+        pref = [0.0] * (n + 1)
+        for i in range(1, n + 1):
+            pref[i] = pref[i - 1] + nums[i - 1]
+        
+        def avg(l, r):
+            return (pref[r + 1] - pref[l]) / (r - l + 1)
+        # dp[g][i] = best score for the first i elements using exactly g groups.
+        dp = [[0.0] * (n + 1) for _ in range(k + 1)]
+
+        # Base: dp[1][i] = average(0..i-1).
+        for i in range(1, n + 1):
+            dp[1][i] = avg(0, i - 1)
+        
+        for g in range(2, k + 1):
+            # need at least g elements to fill g groups
+            for i in range(g, n + 1):
+                best = 0.0
+                #  Last cut at t (so last group is [t..i-1])
+                # # t must be >= g-1 so that first g-1 groups have at least g-1 elements.
+                for t in range(g - 1, i):
+                    candidate = dp[g - 1][t] + avg(t, i - 1)
+                    if candidate > best:
+                        best = candidate
+                dp[g][i] = best
+        return dp[k][n]
+
