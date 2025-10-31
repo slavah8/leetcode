446d3057f9297378d4a8e7c5cb86774d1365e9ba
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-10-31 18:49:01 -0400

    Time: 98 ms (80.79%), Space: 18.2 MB (70.53%) - LeetHub

--- 0831-largest-sum-of-averages/0831-largest-sum-of-averages.py
+++ 0831-largest-sum-of-averages/0831-largest-sum-of-averages.py
@@ -15,13 +15,13 @@ class Solution:
         for i in range(1, n + 1):
             dp[1][i] = avg(0, i - 1)
         
-        for g in range(2, k + 1):
+        for g in range(2, k + 1): # # g = number of groups
             # need at least g elements to fill g groups
-            for i in range(g, n + 1):
+            for i in range(g, n + 1): # # i = prefix length (we solve for first i elements)
                 best = 0.0
                 #  Last cut at t (so last group is [t..i-1])
                 # # t must be >= g-1 so that first g-1 groups have at least g-1 elements.
-                for t in range(g - 1, i):
+                for t in range(g - 1, i): # # t = index where the last group starts
                     candidate = dp[g - 1][t] + avg(t, i - 1)
                     if candidate > best:
                         best = candidate
