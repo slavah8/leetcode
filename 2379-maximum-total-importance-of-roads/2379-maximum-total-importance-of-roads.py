1178a35a95c39a1188493aff6161a288fb2f8306
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-11-15 12:27:10 -0500

    Time: 274 ms (5%), Space: 47 MB (8.25%) - LeetHub

---
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
+        
+        graph = defaultdict(list)
+        node_degree = defaultdict(int)
+        for u, v in roads:
+            graph[u].append(v)
+            graph[v].append(u)
+            node_degree[u] += 1
+            node_degree[v] += 1
+        
+        
+        print(graph)
+        print(node_degree)
+        sorted_nodes = sorted(node_degree, key = lambda node: node_degree[node], reverse = True)
+    
+        # need to assign
+        assign = n
+        node_val = defaultdict(int)
+        for node in sorted_nodes:
+            node_val[node] = assign
+            assign -= 1
+        print(node_val)
+
+        importance = 0
+        for u, v in roads:
+            importance += node_val[u] + node_val[v]
+        
+        return importance
+
+
+        
+
