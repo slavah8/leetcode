e26119ad3ae31b565ef2f804dd357958b07511f8
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-11-25 01:34:06 -0500

    Time: 345 ms (5.88%), Space: 27.4 MB (5.88%) - LeetHub

---
+++ 1634-add-two-polynomials-represented-as-linked-lists/1634-add-two-polynomials-represented-as-linked-lists.py
@@ -0,0 +1,49 @@
+# Definition for polynomial singly-linked list.
+# class PolyNode:
+#     def __init__(self, x=0, y=0, next=None):
+#         self.coefficient = x
+#         self.power = y
+#         self.next = next
+
+class Solution:
+    def addPoly(self, poly1: 'PolyNode', poly2: 'PolyNode') -> 'PolyNode':
+        curr = PolyNode()
+        dummy = PolyNode(next = curr)
+        head1 = poly1
+        head2 = poly2
+        while head1 and head2:
+            if head1.power == head2.power:
+                coeff = head1.coefficient + head2.coefficient 
+                print(coeff)
+                if coeff == 0:
+                    head1 = head1.next
+                    head2 = head2.next
+                else:
+                    new = PolyNode(x = coeff, y = head1.power)
+                    curr.next = new
+                    curr = curr.next
+                    head1 = head1.next
+                    head2 = head2.next     
+            elif head1.power > head2.power:
+                curr.next = head1
+                curr = curr.next
+                head1 = head1.next
+            else:
+                curr.next = head2
+                curr = curr.next
+                head2 = head2.next
+            print(curr.coefficient, curr.power)
+        
+        if head2:
+            curr.next = head2
+        else:
+            curr.next = head1
+
+        print(dummy.next.next)
+        return dummy.next.next
+        
+
+        
+
+
+            
