5760816d97bfbe6340f2f6dde1c2d759c142b36e
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-12-05 22:35:21 -0500

    Time: 1 ms (81.83%), Space: 42.8 MB (25.34%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0;
+        int j = 0;
+
+        while (j < typed.length()) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+                j++;
+                continue;
+            } else if (j > 0 && typed.charAt(j - 1) == typed.charAt(j)) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+
+        return i == name.length();
+    }
+}
