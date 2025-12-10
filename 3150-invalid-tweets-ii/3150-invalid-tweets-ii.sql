e2c5f3c361c1f94cd6162adc38880d34e075e2a3
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2025-12-10 15:04:13 -0500

    Time: 291 ms (25.37%), Space: 0B (100%) - LeetHub

---
+++ 3150-invalid-tweets-ii/3150-invalid-tweets-ii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+
+SELECT
+    tweet_id
+FROM Tweets
+WHERE
+    LENGTH(content) > 140 OR
+    LENGTH(content) - LENGTH(REPLACE(content, '@', '')) > 3 OR
+    LENGTH(content) - LENGTH(REPLACE(content, '#', '')) > 3 
