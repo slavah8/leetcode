625e745957b242d4fb7bab21ffcc304df0742f5f
Slava Hlushko <70113906+slavah8@users.noreply.github.com>
2026-01-23 14:00:28 -0500

    Time: 0 ms (100%), Space: 100.2 MB (91.27%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool inc = true;
+        bool dec = true;
+
+        for (int i = 0; i < (int)nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                inc = false;
+            }
+            if (nums[i] < nums[i + 1]) {
+                dec = false;
+            }
+        }
+        return inc || dec;
+    }
+};
